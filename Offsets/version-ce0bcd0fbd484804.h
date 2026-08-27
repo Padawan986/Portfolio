931a83f983bc026b82074a6291d66891e9ef96d4
Padawan985 <padawan985@gmail.com>
2026-08-27 23:23:03 +0200

    Update version-ce0bcd0fbd484804.h

--- Offsets/version-ce0bcd0fbd484804.h
+++ Offsets/version-ce0bcd0fbd484804.h
@@ -1,7 +1,7 @@
 #pragma once
 /* =============================================================
 /*                       Pawa-Lite External offsets
-/*              https://discord.gg/GDGMtKcARQ
+/*              https://discord.gg/SKGmyMVtKP
 /* -------------------------------------------------------------
 /*  Roblox Version  : version-ce0bcd0fbd484804
 /* =============================================================
