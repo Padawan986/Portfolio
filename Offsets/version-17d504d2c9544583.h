e0de4c9e6201abda03b5ba01211a7db332a43673
Padawan985 <padawan985@gmail.com>
2026-08-27 23:23:17 +0200

    Update Discord link in version header comment

--- Offsets/version-17d504d2c9544583.h
+++ Offsets/version-17d504d2c9544583.h
@@ -1,7 +1,7 @@
 #pragma once
 /* =============================================================
 /* Pawa-Lite External offsets
-/* https://discord.gg/GDGMtKcARQ
+/* https://discord.gg/SKGmyMVtKP
 /* -------------------------------------------------------------
 /* Roblox Version : version-17d504d2c9544583
 /* =============================================================
